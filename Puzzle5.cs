f5d986cc76ee441f56db599d77800be733bf9c93
daveganote <75378411+daveganote@users.noreply.github.com>
2020-12-06 10:16:59 -0700

    Add files via upload

---
+++ Puzzle5.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Puzzle5
+    {
+        public static void SolvePuzzle5_P1(string inputFile)
+        {
+            using (StreamReader sr = new StreamReader(inputFile))
+            {
+                int maxID = int.MinValue;
+                string buffer = sr.ReadLine();
+                while (buffer != null)
+                {
+                    string row = buffer.Remove(buffer.Length - 3);
+                    string seat = buffer.Substring(row.Length);
+                    int rowNo = FindValue(row, 0, 127, 64, 'F');
+                    int seatNo = FindValue(seat, 0, 7, 4, 'L');
+                    int seatID = (8 * rowNo) + seatNo;
+
+                    maxID = Math.Max(seatID, maxID);
+
+                    buffer = sr.ReadLine();
+                }
+                Console.WriteLine($"Max seat ID: {maxID}");
+            }
+        }
+
+        public static int FindValue(string input, double low, double high, double diff, char directionIndicator)
+        {
+            for (int i = 0; i < input.Length -1; i++)
+            {
+                if (input[i] == directionIndicator)
+                    high = high - diff;
+                else
+                    low = low + diff;
+
+                diff = Math.Ceiling((high - low)/2);
+            }
+
+            if (input[input.Length - 1] == directionIndicator)
+                return (int)low;
+            return (int)high;
+        }
+
+        public static void SolvePuzzle5_P2(string inputFile)
+        {
+            using (StreamReader sr = new StreamReader(inputFile))
+            {
+                List<int> seatIDs = new List<int>();
+                int minID = int.MaxValue;
+                int maxID = int.MinValue;
+                string buffer = sr.ReadLine();
+                while (buffer != null)
+                {
+                    string row = buffer.Remove(buffer.Length - 3);
+                    string seat = buffer.Substring(row.Length);
+                    int rowNo = FindValue(row, 0, 127, 64, 'F');
+                    int seatNo = FindValue(seat, 0, 7, 4, 'L');
+                    int seatID = (8 * rowNo) + seatNo;
+
+                    minID = Math.Min(seatID, maxID);
+                    maxID = Math.Max(seatID, maxID);
+
+                    seatIDs.Add(seatID);
+
+                    buffer = sr.ReadLine();
+                }
+
+                for (int i = minID; i < maxID; i++)
+                {
+                    if (!seatIDs.Contains(i) && seatIDs.Contains(i + 1) && seatIDs.Contains(i - 1))
+                    {
+                        Console.WriteLine($"Seat ID: {i}");
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Puzzle6.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public static class Puzzle6
+    {
+        public static void SolvePuzzle6_P1(string inputFile)
+        {
+            using (StreamReader sr = new StreamReader(inputFile))
+            {
+                string buffer = sr.ReadLine();
+                string answers = string.Empty;
+                int totalAnswers = 0;
+                while (buffer!= null)
+                {
+                    if (string.IsNullOrEmpty(buffer))
+                    {
+                        totalAnswers += CountUniqueCharsInString(answers);
+                        answers = string.Empty;
+                    }
+                    else
+                    {
+                        answers += buffer;
+                    }
+                    buffer = sr.ReadLine();
+                }
+                if (!string.IsNullOrEmpty(answers))
+                {
+                    totalAnswers += CountUniqueCharsInString(answers);
+                }
+                Console.WriteLine($"Total unqiue answers in group: {totalAnswers}");
+            }
+        }
+
+        private static int CountUniqueCharsInString(string input)
+        {
+            List<char> uniqueAnswers = new List<char>();
+            foreach (char c in input)
+            {
+                if (!uniqueAnswers.Contains(c))
+                    uniqueAnswers.Add(c);
+            }
+            return uniqueAnswers.Count;
+        }
+
+        public static void SolvePuzzle6_P2(string inputFile)
+        {
+            using (StreamReader sr = new StreamReader(inputFile))
+            {
+                string buffer = sr.ReadLine();
+                int people = 0;
+                string answers = string.Empty;
+                int totalAnswers = 0;
+                while (buffer != null)
+                {
+                    if (string.IsNullOrEmpty(buffer))
+                    {
+                        totalAnswers += CountAllAnsweredYes(answers, people);
+                        answers = string.Empty;
+                        people = 0;
+                    }
+                    else
+                    {
+                        answers += buffer;
+                        people += 1;
+                    }
+                    buffer = sr.ReadLine();
+                }
+                if (!string.IsNullOrEmpty(answers))
+                {
+                    totalAnswers += CountAllAnsweredYes(answers, people);
+                }
+                Console.WriteLine($"Total unqiue answers in group: {totalAnswers}");
+            }
+        }
+
+        private static int CountAllAnsweredYes(string input, int people)
+        {
+            Dictionary<char, int> answers = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (!answers.ContainsKey(c))
+                    answers.Add(c, 1);
+                else
+                    answers[c] += 1;
+            }
+
+            int allAnswered = 0;
+            foreach (char c in answers.Keys)
+            {
+                if (answers[c] == people) allAnswered += 1;
+            }
+            return allAnswered;
+        }
+    }
+}
